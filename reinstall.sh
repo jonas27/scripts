6519942002df797a3492f7fe64daafa4db6e10e9
jonas27 <joE@12345>
2019-07-11 12:33:32 +0200

    Commit from shell script

--- reinstall.sh
+++ reinstall.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 # apt install stuff
-sudo apt install -y git chrome-gnome-shell
+sudo apt install -y git chrome-gnome-shell tilix
 
 # apt install stuff
 sudo snap install -y code --classic
+
+# do synbolic link stuff
+ln -s $HOME/git/scripts $HOME/scripts
+
+# make all scripts executable
+chmod +x $HOME/git/scripts/*.sh
