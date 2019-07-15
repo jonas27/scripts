9f9047c6bd3e5a5966ae96199e9acaccd52a39f3
jonas27 <joE@12345>
2019-07-15 16:35:29 +0200

    Commit from shell script

--- pi/k8sDepInst.sh
+++ pi/k8sDepInst.sh
@@ -3,6 +3,9 @@
 # Fixes some issues concerning local variables and cgroups
 export LC_ALL=C
 
+# Allow for all releases in buster release
+sudo apt-get update --allow-releaseinfo-change
+
 # Turn swap off
 sudo dphys-swapfile swapoff && sudo dphys-swapfile uninstall && sudo update-rc.d dphys-swapfile remove
 
@@ -12,13 +15,14 @@ sudo sed -i '$a cgroup_enable=cpuset cgroup_enable=memory' /boot/cmdline.txt
 # Install docker
 curl -s https://download.docker.com/linux/raspbian/gpg | sudo apt-key add -
 echo "deb [arch=armhf] https://download.docker.com/linux/raspbian stretch edge" | sudo tee /etc/apt/sources.list.d/socker.list
-sudo apt-get update -q
-sudo apt-get install -y docker-ce=18.06.0~ce~3-0~raspbian --allow-downgrades
+sudo apt update -q
+sudo apt install -y docker-ce=18.06.0~ce~3-0~raspbian --allow-downgrades
 echo "docker-ce hold" | sudo dpkg --set-selections
 sudo usermod pi -aG docker
 
 # Install kubeadm
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get update -q && sudo apt-get install -y kubeadm kubectl kubelet
-sudo reboot
+sudo apt update -q && sudo apt install -y kubeadm kubectl kubelet
+
+# sudo reboot
