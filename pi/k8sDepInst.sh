1ee5d5dbc2b4ba6aa6b14180807455eeac602a62
jonas27 <joE@12345>
2019-07-16 09:53:35 +0200

    Commit from shell script

--- pi/k8sDepInst.sh
+++ pi/k8sDepInst.sh
@@ -4,7 +4,7 @@
 export LC_ALL=C
 
 # Allow for all releases in buster release
-sudo apt-get update --allow-releaseinfo-change
+# sudo apt-get update --allow-releaseinfo-change
 
 # Turn swap off
 sudo dphys-swapfile swapoff && sudo dphys-swapfile uninstall && sudo update-rc.d dphys-swapfile remove
@@ -12,26 +12,26 @@ sudo dphys-swapfile swapoff && sudo dphys-swapfile uninstall && sudo update-rc.d
 # give all cgroups access to cpuset and memory. Important for K3s
 sudo sed -i '$a cgroup_enable=cpuset cgroup_enable=memory' /boot/cmdline.txt
 
-# Install docker did not work
-# curl -s https://download.docker.com/linux/raspbian/gpg | sudo apt-key add -
-# echo "deb [arch=armhf] https://download.docker.com/linux/raspbian stretch edge" | sudo tee /etc/apt/sources.list.d/socker.list
-# sudo apt update -q
-# sudo apt install -y docker-ce=18.09.0~3-0~raspbian-stretch --allow-downgrades --no-install-recommends
-# echo "docker-ce hold" | sudo dpkg --set-selections
-# sudo usermod pi -aG docker
-# try change from 18.06.1~ce~3-0~raspbian to 18.09.0~3-0~raspbian-stretch
-# did not work
-sudo apt-get update
-apt-get install -y apt-transport-https ca-certificates software-properties-common
-curl -fsSL https://yum.dockerproject.org/gpg | sudo apt-key add -
-echo "deb https://download.docker.com/linux/raspbian/ stretch stable" > /etc/apt/sources.list.d/docker.list
-apt -y install  docker-ce
-systemctl enable docker.service
-systemctl start docker.service
+# Install docker/kubeadm dependencies
+sudo apt-get install apt-transport-https ca-certificates software-properties-common -y
 
-# Install kubeadm
+# Install docker 
+curl -fsSL get.docker.com -o get-docker.sh && sh get-docker.sh
+# Give user docker rights
+sudo usermod -aG docker pi
+# Import Docker CPG key
+sudo curl https://download.docker.com/linux/raspbian/gpg
+# Setup the Docker Repo.
+sudo sed -i '$a deb https://download.docker.com/linux/raspbian/ stretch stable' /etc/apt/sources.list
+# Fetch and upgrade
+sudo apt-get update
+sudo apt-get upgrade -y
+# Init docker
+sudo systemctl start docker.service
+
+# Install k8s
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt update -q && sudo apt install -y kubeadm kubectl kubelet
 
-# sudo reboot
+sudo reboot
